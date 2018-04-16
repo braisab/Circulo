947fda3d3a54ed454adad34e8c401dd56c4d6d05
alacazadeltutorial <37708291+alacazadeltutorial@users.noreply.github.com>
2018-04-16 10:14:35 +0200

    Update Main.java

--- src/circulo/Main.java
+++ src/circulo/Main.java
@@ -3,25 +3,25 @@ package circulo;
 import java.text.DecimalFormat;
 
 /*
- * Clase Main para hacer pruebas en NetBeans con la 
+ * Clase Main para facer probas en NetBeans coa 
  * clase Circulo
  */
 public class Main {
     public static void main(String[] args) {
         Circulo circulo = new Circulo(37,43,2.5);
-        String salida =
-                "La coordenada X es "+circulo.obtenerX()+
-                "\nLa coordenada Y es "+circulo.obtenerY()+
-                "\nEl radio es "+circulo.obtenerRadio();
+        String saida =
+                "A coordenada X é "+circulo.obterX()+
+                "\nA coordenada Y é "+circulo.obterY()+
+                "\nO radio é "+circulo.obterRadio();
         circulo.establecerX(35);
         circulo.establecerY(20);
         circulo.establecerRadio(4.2);
-        salida+="\n\nLa nueva ubicación y el radio de círculo son\n"+circulo.toString();
-        DecimalFormat dosDigitos = new DecimalFormat("0.00");
-        salida+="\nEl diámetro es "+dosDigitos.format(circulo.obtenerDiametro());
-        salida+="\nLa circunferencia es "+dosDigitos.format(circulo.obtenerCircunferencia());
-        salida+="\nEl área es "+dosDigitos.format(circulo.obtenerArea());
-        System.out.println(salida);
+        saida+="\n\nA nova ubicación e o radio de círculo son\n"+circulo.toString();
+        DecimalFormat dousDixitos = new DecimalFormat("0.00");
+        saida+="\nO diámetro é "+dousDixitos.format(circulo.obterDiametro());
+        saida+="\nA circunferencia é "+dousDixitos.format(circulo.obterCircunferencia());
+        saida+="\nA área é "+dousDixitos.format(circulo.obterArea());
+        System.out.println(saida);
         System.exit(0);
     }
 
